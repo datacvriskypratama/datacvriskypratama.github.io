f47e00464502020108ba2114d24e0325bdf802bb
datacvriskypratama <81645433+datacvriskypratama@users.noreply.github.com>
2023-08-17 02:59:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
 if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
 
 define('DEFAULT_CONTROLLER', 'C_Auth');
-define('BASE_URL', 'http://localhost/rental-mobil/');
+define('BASE_URL', 'https://databases.000webhost.com/index.php?route=/database/sql&db=id20226576_datacvrp22');
 define('APP_NAME', 'Aplikasi Rental Mobil');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'rental_mobil');
+define('DB_USER', 'id20226576_datacvrp22');
+define('DB_PASS', 'Akbar321@@');
+define('DB_NAME', 'id20226576_datacvrp22');
 
 ?>
