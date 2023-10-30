951faab1c4305f0af967bd7adf5ce018e19cd838
datacvriskypratama <81645433+datacvriskypratama@users.noreply.github.com>
2023-10-31 02:49:27 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+});
